c18de012265a38dd0cb541ed21b4d9519c111d3e
tdnine <adityanr4@gmail.com>
2022-08-07 16:50:34 +0530

    added mark as close and get ticket info routes

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -1,4 +1,5 @@
 let debug = require('debug')('ticketing: ticketController')
+const User = require('../model/userModel')
 const Ticket = require('../model/ticketModel')
 const { validationResult } = require('express-validator')
 
@@ -15,8 +16,13 @@ exports.newTicket = async (req, res, next) => {
         //GET BODY DATA//
         const { title, description } = req.body
 
+        //ASSIGN TICKET TO RANDOMLY SELECTED EMPLOYEE
+        const employees = await User.find({role: 'employee'}).select('username')
+        const employeeIds = employees.map(item => item._id)
+        const randomId = Math.floor((Math.random() * employeeIds.length))
+
         //SAVE NEW USER TO DB
-        const newTicket = new Ticket({ title, description })
+        const newTicket = new Ticket({ title, description, assignedTo: employeeIds[randomId] })
         const createdTicket = await newTicket.save()
 
         //RETURN AUTH TOKEN
@@ -25,3 +31,65 @@ exports.newTicket = async (req, res, next) => {
         next(err)
     }    
 }
+
+exports.ticketList = async(req, res, next) => {
+    try{        
+        
+        //CHECK IF QUERY
+        const {status, title, priority} = req.query
+        
+        //INTIATE FILTER
+        let filter 
+        
+        //ASSIGN FILTER OBJECT ACCORDING TO QUERY ELSE, EMPTY FILTER OBJECT
+        (status) ? filter = {status} 
+        : (title) ? filter = {title}
+        : (priority) ? filter = {priority}        
+        : filter = {}
+
+        //GET TICKETS ACCORDING TO FILTER
+        const tickets = await Ticket.find(filter)
+        return res.status(200).json(tickets)
+
+    }catch(err){
+        next(err)
+    }
+}
+
+exports.closeTicket = async(req, res, next) => {
+    try{
+        const {ticketId} = req.params
+
+        //GET TICKET INFO OF TICKET ID
+        const ticket = await Ticket.findById(ticketId)
+        const currentTicketPriority = ticket.priority        
+        
+        //PRIORITY CHECK
+        
+        //GET ALL TICKETS OF THAT EMPLOYEE MINUS THE CURRENT TICKET
+        const employeeId = ticket.assignedTo
+        const employeeTickets = await Ticket.find({assignedTo: employeeId})
+        
+        const priorityCheckList = employeeTickets.filter(item => item._id != ticketId).map(item => item.priority)
+        debug(priorityCheckList)        
+        
+        //CHECK IF CURRENT TICKET IS LOWER THAN ALL OTHER TICKETS
+        let higherPriorityTickets = []
+        if(currentTicketPriority == 'low' && priorityCheckList.includes('medium') || priorityCheckList.includes('high')){
+            higherPriorityTickets = employeeTickets.filter(item => item.priority != 'low')            
+        }else if(currentTicketPriority == 'medium' && priorityCheckList.includes('high')){
+            higherPriorityTickets = employeeTickets.filter(item => item.priority == 'high')            
+        }
+        
+        if(higherPriorityTickets.length > 0)
+            return res.status(400).json({msg: 'A higher priority task remains to be closed', tickets: higherPriorityTickets})
+        
+        //CLOSE TICKET
+        await Ticket.findByIdAndUpdate(ticketId, {status: 'close'})
+        res.status(200).json({msg: 'Ticket closed successfully'})
+
+    }catch(err){
+        next(err)
+    }
+}
+
--- routes/ticket.js
+++ routes/ticket.js
@@ -6,4 +6,10 @@ const {newTicketValidator} = require('../validations/ticketValidation')
 
 router.post('/new', verifyToken, authorizeRole(['admin']), newTicketValidator, ticketController.newTicket)
 
+router.get('/all', ticketController.ticketList)
+
+router.get('/', ticketController.ticketList)
+
+router.post('/:ticketId/markasclosed', verifyToken, authorizeRole(['admin', 'employee']), ticketController.closeTicket)
+
 module.exports = router
